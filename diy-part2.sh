0cfed695a1d57e8c7498dc6feab0e6a962c97e18
mzfffff <121182546+mzfffff@users.noreply.github.com>
2024-12-02 21:39:39 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,10 @@
 # See /LICENSE for more information.
 #
 
+curl -s https://raw.githubusercontent.com/sbwml/r4s_build_script/4a9fafefd67172e074fa62cbe3570c4e197376b3/openwrt/patch/apk-tools/9999-hack-for-linux-pre-releases.patch > package/system/apk/patches/9999-hack-for-linux-pre-releases.patch
+
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
